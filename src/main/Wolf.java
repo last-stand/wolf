c04705add4e821a9932d69734ca18ce34327c147
Jai <jplko.simt@gmail.com>
2016-07-28 13:26:58 +0530

    JP | gradle task for giving input from commandLine for file or input string is added.

--- src/main/Wolf.java
+++ src/main/Wolf.java
@@ -1,16 +1,25 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.io.InputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 public class Wolf {
     public static void main(String[] args) throws Exception {
         String inputFile = null;
 
-        if (args.length > 0 ) inputFile = args[0];
+        if (args.length > 0 )
+          inputFile = args[0];
+        else
+          System.out.println("Enter input string to parse: ");
 
         InputStream is = System.in;
-        if (inputFile != null ) is = new FileInputStream(inputFile);
+        if (inputFile != null ){
+          File file = new File(inputFile);
+          is = file.exists() ? new FileInputStream(inputFile) : new ByteArrayInputStream(inputFile.getBytes(StandardCharsets.UTF_8));
+        }
         ANTLRInputStream inputStream = new ANTLRInputStream(is);
         WolfLexer lexer = new WolfLexer(inputStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
